efd6cb4d7c0d34d3edecb9e566e1792c36112087
Dean Nash <shibta007@gmail.com>
2022-11-30 15:00:45 +0200

    Fixes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const app = require("./routes/app");
 
-const { PORT = 3000, DBURL = "mongodb://localhost:27017/whynoenv" } =
+const { PORT = 3000, DBURL = "mongodb://localhost:27017/whyunoenv" } =
   process.env;
 
 mongoose.connect(DBURL);
